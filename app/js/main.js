791aad2e30b61f88e3274f2adc963e08e3510bd1
veS-Sev <newsecretword@yandex.ru>
2020-02-25 17:44:18 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -38,6 +38,18 @@ $('.icon-th-large').on('click',function(){
 });
 
 
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
+
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+
+
+
+
   var mixer = mixitup('.products__inner-box');
 
 
